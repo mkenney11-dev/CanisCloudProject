f461568fb4dc02c4c3e143c4b954a26c7691fd4b
Maggie_Cydeo <maggie.kenney115@gmail.com>
2023-10-25 13:14:11 -0400

    Added US feature files and login pages

---
+++ src/test/java/com/sprint1Implementation/step_definitions/login.java
@@ -0,0 +1,21 @@
+package com.sprint1Implementation.step_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class login {
+
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+
+    }
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+
+    }
+    @Given("the user logged in with username as {string} and password as {string}")
+    public void the_user_logged_in_with_username_as_and_password_as(String string, String string2) {
+
+    }
+
+}
---
+++ src/test/resources/features/US008_ContactGroupFunctionality.feature
@@ -0,0 +1,5 @@
+Feature:
+
+  Background:
+
+    Scenario:
